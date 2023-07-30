919fd324e2acc3cc4c89ca3ceacbc053a5e206f6
anurag11032000 <81571874+anurag11032000@users.noreply.github.com>
2023-07-30 16:25:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+function myFunction1(){
+   var n1= document.getElementById("t1").value;
+   var n2=document.getElementById("t2").value;
+   var num1=parseInt(n1);
+   var num2=parseInt(n2);
+   var sum=num1+num2;
+   document.getElementById("r1").innerHTML=sum;
+}
+function myFunction2(){
+   var n1= document.getElementById("t1").value;
+   var n2=document.getElementById("t2").value;
+   var num1=parseInt(n1);
+   var num2=parseInt(n2);
+   var sum=num1-num2;
+   document.getElementById("r1").innerHTML=sum;
+}
+
+function myFunction3(){
+   var n1= document.getElementById("t1").value;
+   var n2=document.getElementById("t2").value;
+   var num1=parseInt(n1);
+   var num2=parseInt(n2);
+   var sum=num1/num2;
+   document.getElementById("r1").innerHTML=sum;
+}
+
+
+    
+function myFunction4(){
+   var n1= document.getElementById("t1").value;
+   var n2=document.getElementById("t2").value;
+   var num1=parseInt(n1);
+   var num2=parseInt(n2);
+   var sum=num1*num2;
+   document.getElementById("r1").innerHTML=sum;
+}
+
+
